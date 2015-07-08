90955623d758335b2aafe730c498de2bc4c96f1a
deepmistry <deepdmistry@gmail.com>
2015-07-08 18:23:11 -0400

    Added logging level enum - might delete this later.

--- src/main/java/ILogger.java
+++ src/main/java/ILogger.java
@@ -2,7 +2,87 @@
  * Description: Main interface to specify the behavior of the wrapper to log4j.
  *
  * @author Created by deepmistry
- *         6/24/15.
+ * 6/24/15.
  */
 public interface ILogger {
+
+    /**
+     * Error level log entry
+     *
+     * @param message The message to log
+     */
+    void error(String message);
+    /**
+     * Error level log entry
+     *
+     * @param message The message to log
+     * @param e The exception that occurred
+     */
+    void error(String message, Exception e);
+    /**
+     * Warn level log entry
+     *
+     * @param message The message to log
+     */
+    void warn(String message);
+    /**
+     * Error level log entry
+     *
+     * @param message The message to log
+     * @param e The exception that occurred
+     */
+    void warn(String message, Exception e);
+    /**
+     * Info level log entry
+     *
+     * @param message The message to log
+     */
+    void info(String message);
+    /**
+     * Error level log entry
+     *
+     * @param message The message to log
+     * @param e The exception that occurred
+     */
+    void info(String message, Exception e);
+    /**
+     * Debug level log entry
+     *
+     * @param message The message to log
+     */
+    void debug(String message);
+    /**
+     * Error level log entry
+     *
+     * @param message The message to log
+     * @param e The exception that occurred
+     */
+    void debug(String message, Exception e);
+    /**
+     * Debug High level log entry
+     *
+     * @param message The message to log
+     */
+    void debugHigh(String message);
+    /**
+     * Error level log entry
+     *
+     * @param message The message to log
+     * @param e The exception that occurred
+     */
+    void debugHigh(String message, Exception e);
+    /**
+     * Trace level log entry
+     *
+     * @param message The message to log
+     */
+    void trace(String message);
+    /**
+     * Error level log entry
+     *
+     * @param message The message to log
+     * @param e The exception that occurred
+     */
+    void trace(String message, Exception e);
+
 }
---
+++ src/main/java/Logger.java
@@ -0,0 +1,56 @@
+/**
+ * Description:
+ *
+ * @author Created by deepmistry
+ *         6/27/15.
+ */
+public class Logger implements ILogger {
+
+    public void error(String message) {
+
+    }
+
+    public void error(String message, Exception e) {
+
+    }
+
+    public void warn(String message) {
+
+    }
+
+    public void warn(String message, Exception e) {
+
+    }
+
+    public void info(String message) {
+
+    }
+
+    public void info(String message, Exception e) {
+
+    }
+
+    public void debug(String message) {
+
+    }
+
+    public void debug(String message, Exception e) {
+
+    }
+
+    public void debugHigh(String message) {
+
+    }
+
+    public void debugHigh(String message, Exception e) {
+
+    }
+
+    public void trace(String message) {
+
+    }
+
+    public void trace(String message, Exception e) {
+
+    }
+}
